590588160b0a275e5a59f336e5a7270152ee5f97
Juan Carlos Díaz Vélez <juan.diazvelez@alumnos.udg.mx>
2019-05-22 15:36:32 -0500

    Updated include statements for new healpix

--- iter-lhreco/src/src/reconstruct.cc
+++ iter-lhreco/src/src/reconstruct.cc
@@ -1,11 +1,26 @@
+
+#ifdef ICECUBE
 #include <healpix-cxx/healpix/alm.h>
 #include <healpix-cxx/cxxsupport/xcomplex.h>
 #include <healpix-cxx/healpix/alm_healpix_tools.h>
 #include <healpix-cxx/cxxsupport/fitshandle.h>
 #include <healpix-cxx/healpix/healpix_map.h>
 #include <healpix-cxx/healpix/healpix_map_fitsio.h>
+#define MyDTYPE FITSUTIL<double>::DTYPE 
+
+#else
+#include <healpix_cxx/alm.h>
+#include <healpix_cxx/xcomplex.h>
+#include <healpix_cxx/alm_healpix_tools.h>
+#include <healpix_cxx/fitshandle.h>
+#include <healpix_cxx/healpix_map.h>
+#include <healpix_cxx/healpix_map_fitsio.h>
+#define MyDTYPE PLANCK_FLOAT64
+#endif //ICECUBE
+
 #include <math.h>
 #include <numeric>
+
 
 #include <boost/shared_ptr.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
@@ -31,13 +46,11 @@
 #include <hawcnest/Logging.h>
 #include <hawcnest/CommandLineConfigurator.h>
 #include <detector-service/ConfigDirDetectorService.h>
-#define MyDTYPE PLANCK_FLOAT64
 
 #else
 namespace po = boost::program_options; 
 #define log_info(args...) cout << args << endl
 #define log_fatal(args...) throw args
-#define MyDTYPE FITSUTIL<double>::DTYPE 
 #endif
 
 
--- iter-lhreco/src/src/significance.cc
+++ iter-lhreco/src/src/significance.cc
@@ -1,9 +1,22 @@
+#ifdef ICECUBE
 #include <healpix-cxx/healpix/alm.h>
 #include <healpix-cxx/cxxsupport/xcomplex.h>
 #include <healpix-cxx/healpix/alm_healpix_tools.h>
 #include <healpix-cxx/cxxsupport/fitshandle.h>
 #include <healpix-cxx/healpix/healpix_map.h>
 #include <healpix-cxx/healpix/healpix_map_fitsio.h>
+#define MyDTYPE FITSUTIL<double>::DTYPE 
+
+#else
+#include <healpix_cxx/alm.h>
+#include <healpix_cxx/xcomplex.h>
+#include <healpix_cxx/alm_healpix_tools.h>
+#include <healpix_cxx/fitshandle.h>
+#include <healpix_cxx/healpix_map.h>
+#include <healpix_cxx/healpix_map_fitsio.h>
+#define MyDTYPE PLANCK_FLOAT64
+#endif //ICECUBE
+
 #include <math.h>
 #include <numeric>
 
