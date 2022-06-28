00d106dae4c6006a26a875014f5fcab4b131b3f4
Juan Carlos Diaz-Velez <juan.diazvelez@alumnos.udg.mx>
2022-06-28 09:39:31 -0500

    Added second-order correction to solar dipole

--- TimeScramble/src/include/solardipole.h
+++ TimeScramble/src/include/solardipole.h
@@ -7,8 +7,20 @@
 #define degrees M_PI/180.
 
 
+/*
+ * solar dipole correction from theory 
+ * \frac{\Delta I}{I} = (\gamma + 2) \frac{v}{c} cos(\theta)
+ */
+double solar_dipole(double mjd, double ra, double dec, bool soc=true);
 
-double solar_dipole(double mjd, double ra, double dec);
+
+/*
+ * Second-order solar dipole correction (taken empirically from data) accounts
+ * for angular resolution as a function of declination (zenith angle)
+ */
+double solar_2nd_order(double dec);
+
+
 
 
 #endif
