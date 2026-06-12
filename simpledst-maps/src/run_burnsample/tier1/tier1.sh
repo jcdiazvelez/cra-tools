8ec7edff7f98b58e0f0345c84c7e71cc8dc9b2ec
Alex McClure <amcclure@icecube.wisc.edu>
2026-06-12 13:41:06 -0500

    Update tier1.sh
    Remove --sd flag, changed dates so they span a full run year

--- simpledst-maps/src/run_burnsample/tier1/tier1.sh
+++ simpledst-maps/src/run_burnsample/tier1/tier1.sh
@@ -5,7 +5,7 @@ function run_makemaps {
     options=$2
 
     # Run the command to produce the dag scripts.
-    command="python ../../scripts/make-local-maps-IT.py.in -c ITpass2 -o /data/ana/CosmicRay/Anisotropy/IceTop/ITpass2/output/@USER_DIR@/tier1 --sd $options"
+    command="python ../../scripts/make-local-maps-IT.py.in -c ITpass2 -o /data/user/@USER_DIR@/tier1 $options"
     $command
 
     # Submit the dag files for each year to condor. This will produce 360 fits files for each day in this tier.
@@ -15,10 +15,10 @@ function run_makemaps {
 
 # Define the options for each year
 declare -A options_per_year
-options_per_year[2011]="-d 20110514 20120512 -i --smin 3 --smax 5 --submit_dir ./submit_2011"
-options_per_year[2012]="-d 20120513 20130430 -i --smin 3 --smax 5 --submit_dir ./submit_2012"
-options_per_year[2013]="-d 20130501 20140506 -i --smin 3 --smax 5 --submit_dir ./submit_2013"
-options_per_year[2014]="-d 20140507 20150515 -i --smin 3 --smax 5 --submit_dir ./submit_2014"
+options_per_year[2011]="-d 20110513 20120512 -i --smin 3 --smax 5 --submit_dir ./submit_2011"
+options_per_year[2012]="-d 20120513 20130512 -i --smin 3 --smax 5 --submit_dir ./submit_2012"
+options_per_year[2013]="-d 20130513 20140512 -i --smin 3 --smax 5 --submit_dir ./submit_2013"
+options_per_year[2014]="-d 20140513 20150512 -i --smin 3 --smax 5 --submit_dir ./submit_2014"
 
 # Iterate over 10 years
 for year in {2011..2014}; do
